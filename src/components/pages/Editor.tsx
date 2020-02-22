0179aadf0e4e9ac295b5f42b426d20590ebb148c
松谷 健太朗 <matsutani@geisia.jp>
2020-02-22 20:07:33 +0900

    iframeでサンプルhtmlを追加

--- src/components/pages/Editor.tsx
+++ src/components/pages/Editor.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import html from "sources/html";
+
+const Container = styled.div`
+  background-color: #f5f5f5;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IframeWrap = styled.div`
+  width: 80%;
+  height: 80%;
+  border: 1px solid #ddd;
+  background-color: #fff;
+`;
 
 const Editor: React.FC = () => {
-  return <div>editor</div>;
+  return (
+    <Container>
+      <IframeWrap>
+        <iframe
+          title="iframe"
+          id="iframe"
+          srcDoc={html}
+          width="100%"
+          height="100%"
+        />
+      </IframeWrap>
+    </Container>
+  );
 };
 
 export default Editor;
---
+++ src/sources/html.ts
@@ -0,0 +1,92 @@
+export default `
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>sample</title>
+    <link href="./source.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="page">
+      <header>
+        <div id="header_inner">
+          <div id="header_logo">
+            <img src="https://d2l930y2yx77uc.cloudfront.net/production/uploads/images/6432837/picture_pc_9a06a019fe451a6c54a064d465d927c5.jpg">
+          </div>
+          <div id="header_contact">
+            <a href="#" id="header_contact_inner">
+              <p>お問い合わせ</p>
+            </a>
+          </div>
+          <div id="header_text">
+            <p>TEL　00-0000-0000<br>
+              (受付時間　平日　9:00～17:00)</p>
+          </div>
+        </div>
+      </header>
+      <div id="main_image">
+        <div id="main_image_inner">
+          <h1>キャッチコピー</h1>
+        </div>
+      </div>
+      <main>
+        <section id="section01">
+          <div class="scroll">
+            <h2>テキストテキストテキスト</h2>
+          </div>
+          <nav>
+            <ul id="main_nav">
+              <li>
+                <a href="#div01">1.テキストテキ<br>ストテキスト</a>
+              </li>
+              <li>
+                <a href="#div03">3.テキストテキ<br>ストテキスト</a>
+              </li>
+              <li>
+                <a href="#div02">2.テキストテキ<br>ストテキスト</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="contents" id="div01">
+            <div class="left" id="photo01"></div>
+            <div class="right text">
+              <h4>1.テキストテキストテキストテキスト</h4>
+              <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+          </div>
+          <div class="contents" id="div02">
+            <div class="left text">
+              <h4>2.テキストテキストテキストテキスト</h4>
+              <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+            <div class="right" id="photo02"></div>
+          </div>
+          <div class="contents" id="div03">
+            <div class="left" id="photo03"></div>
+            <div class="right text">
+              <h4>3.テキストテキストテキストテキスト</h4>
+              <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+          </div>
+          <div class="main_contact">
+            <div class="contact_left main_contact_inner">
+              <div class="contact_left_inner">
+                <p>TEL 00-0000-0000<br>
+                  <span>(受付時間 平日 9:00～17:00)</span>
+                </p>
+              </div>
+            </div>
+            <div class="contact_right main_contact_inner">
+              <a class="contact_right_inner" href="#">
+                <p>問い合わせボタン</p>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; samplesamplesamplesamplesample</p>
+      </footer>
+    </div>
+  </body>
+</html>`;
